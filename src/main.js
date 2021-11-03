cc27db7e0a6583f28fc07f4468216906be4f8074
76819 <1728381749@qq.com>
2021-11-03 13:19:22 +0800

    样式添加

--- src/main.js
+++ src/main.js
@@ -4,9 +4,18 @@ import router from './router'
 import store from './store'
 import installElementPlus from './plugins/element'
 import './assets/css/icon.css'
-const app = createApp(App)
-installElementPlus(app)
-app
+
+import axios from "axios";
+import VueAxios from "vue-axios";
+// 全局引入ElementPlus，当然也可以使用局部引入
+import ElementPlus from "element-plus";
+import "element-plus/lib/theme-chalk/index.css"; //引入样式
+import locale from "element-plus/lib/locale/lang/zh-cn";
+
+
+createApp(App)
+    .use(VueAxios,axios)
+    .use(ElementPlus, { size: "small", zIndex: 3000, locale })
     .use(store)
     .use(router)
     .mount('#app')

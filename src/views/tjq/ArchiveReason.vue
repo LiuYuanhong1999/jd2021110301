f90aacdf4719c5cd57b4354d44473fac04a1c83b
WT-20201215FVZB\Administrator <3023074475@qq.com>
2021-11-06 15:07:29 +0800

    更新

--- src/views/tjq/ArchiveReason.vue
+++ src/views/tjq/ArchiveReason.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="top">
-      <el-button type="primary" size="mini">新增</el-button>
+      <el-button type="primary" @click="dialogaddVisible=true" size="mini">新增</el-button>
       <el-button type="primary" size="mini">删除</el-button>
       <el-button type="primary" size="mini">刷新</el-button>
     </div>
@@ -70,7 +70,21 @@
       </el-descriptions>
     </el-dialog>
     <el-dialog v-model="dialogaddVisible" title="新增">
-
+      <el-form  :model="entity" label-width="100px" >
+        <el-form-item label="归档原因">
+          <el-input v-model="entity.acCause" type="text"/>
+        </el-form-item>
+        <el-form-item label="归档类型">
+          <el-select v-model="entity.acType" placeholder="Activity zone">
+            <el-option label="收车归档" value="1"></el-option>
+            <el-option label="销售归档" value="2"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm">确定</el-button>
+          <el-button @click="resetForm">取消</el-button>
+        </el-form-item>
+      </el-form>
     </el-dialog>
   </div>
 </template>
@@ -107,9 +121,26 @@ export default {
 
       },
       dialogaddVisible:false,//新增弹出
+      entity:{//新增实体类
+          acCause:undefined,
+          acType:undefined,
+      }
     }
   },
   methods:{
+    //确定新增
+    submitForm(){
+      this.entity.acType=1;
+      console.log("添加实体",this.entity);
+      this.axios.post("http://localhost:8088/ggarchive/add-w",this.entity)
+      .then(v=>{
+        this.$message("111")
+      })
+    },
+    //取消新增
+    resetForm(){
+
+    },
     //查询详情
     look(){
       this.dialogVisible=true;
